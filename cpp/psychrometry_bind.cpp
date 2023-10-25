1022e49f7a509b7387f0b9586a93b65cd97cb05f
Gökhan Bingöl <gbingol@hotmail.com>
2023-10-25 20:05:36 +0200

    changes to binding

--- cpp/psychrometry_bind.cpp
+++ cpp/psychrometry_bind.cpp
@@ -7,41 +7,22 @@
 
 using namespace emscripten;
 
-std::vector<double> addVectors(const std::vector<double>& vector1, const std::vector<double>& vector2) {
-    if (vector1.size() != vector2.size()) 
-        throw std::runtime_error("Vector sizes must be the same");
 
-    std::vector<double> result(vector1.size());
 
-    for (size_t i = 0; i < vector1.size(); ++i) {
-        result[i] = vector1[i] + vector2[i];
-    }
-
-    return result;
-}
-
-std::string sayhello()
+std::string to_str(
+	const std::vector<std::string>& Keys,
+	const std::vector<double>& Vals)
 {
-    std::vector<std::string> Keys;
-    Keys.push_back("p");
-    Keys.push_back("tdb");
-    Keys.push_back("twb");
+	Psychrometry psy;
+	psy.Compute(Keys, Vals);
 
-    std::vector<double> Vals;
-    Vals.push_back(101325);
-    Vals.push_back(30);
-    Vals.push_back(15);
-
-    Psychrometry psy;
-    psy.Compute(Keys, Vals);
-
-    return psy.to_str();
+	return psy.to_str();
 }
 
 
 EMSCRIPTEN_BINDINGS(my_module) 
 {
     register_vector<double>("VectorDouble");
-    function("addVectors", &addVectors);
-    function("sayhello", &sayhello);
+	register_vector<std::string>("VectorString");
+    function("to_str", &to_str);
 }
--- cpp/test.js
+++ cpp/test.js
@@ -6,16 +6,14 @@ const Module = require('./psychrometry.js'); // Replace with the correct path to
 
 Module().then(myModule => {
 	const v1 = new myModule.VectorDouble();
-	v1.push_back(1.1);
-	v1.push_back(2.2);
+	v1.push_back(101325);
+	v1.push_back(30);
+	v1.push_back(15);
 
-	const v2 = new myModule.VectorDouble();
-	v2.push_back(3.1);
-	v2.push_back(4.2);
-	//v2.push_back(5.2);
-
-    const result = myModule.addVectors(v1, v2);
-    
-	console.log(result.size());
-	console.log(myModule.sayhello());
+	const v2 = new myModule.VectorString();
+	v2.push_back("p");
+	v2.push_back("tdb");
+	v2.push_back("twb");
+	
+	console.log(myModule.to_str(v2, v1));
 });
