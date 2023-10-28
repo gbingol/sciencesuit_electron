ea796065117cf5f8bfbb8868514b34d3bd638dd8
Gökhan Bingöl <gbingol@hotmail.com>
2023-10-28 15:03:34 +0200

    bug fixes

--- cpp/test.js
+++ cpp/test.js
@@ -5,6 +5,7 @@
 const Module = require('./psychrometry.js'); // Replace with the correct path to the generated JavaScript file
 
 Module().then(myModule => {
+	console.log(myModule);
 	const v1 = new myModule.VectorDouble();
 	v1.push_back(101325);
 	v1.push_back(30);
@@ -13,7 +14,7 @@ Module().then(myModule => {
 	const v2 = new myModule.VectorString();
 	v2.push_back("p");
 	v2.push_back("tdb");
-	v2.push_back("tb");
+	v2.push_back("twb");
 	
 	console.log(myModule.to_str(v2, v1));
 });
