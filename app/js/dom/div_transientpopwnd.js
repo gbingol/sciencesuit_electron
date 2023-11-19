be7fa3221e154ac9aed246a7594a65e6b6d035e9
Gökhan Bingöl <gbingol@hotmail.com>
2023-11-19 15:52:18 +0100

    new member func

--- app/js/dom/div_transientpopwnd.js
+++ app/js/dom/div_transientpopwnd.js
@@ -1,6 +1,11 @@
 /*
-	A copyable and deleteable div element
-	When copy clicked, it also shows a transient popup window
+	A transient popup window
+	By default it shows for 2500 ms at the bottom-center and then destroyed
+
+	Usage:
+	let msgBox = document.createElement("div-transientpopwnd");
+	msgBox.innerHTML = "Copied to clipboard";
+	msgBox.timeout=3000; //if another 
 */
 
 class DivTransientPopupWindow extends HTMLElement 
@@ -8,6 +13,8 @@ class DivTransientPopupWindow extends HTMLElement
 	constructor() 
 	{
 		super();
+
+		this.duration = 2500; //ms
 
 		this.shadow = this.attachShadow({ mode: "open" });
 	
@@ -26,12 +33,17 @@ class DivTransientPopupWindow extends HTMLElement
 
 	connectedCallback()
 	{
-		setTimeout(() => { this.shadow.removeChild(this.msgBox); }, 2500);
+		setTimeout(() => { this.shadow.removeChild(this.msgBox); }, this.duration);
 	}
 
 	set innerHTML(val)
 	{
 		this.contentTxt.innerHTML = val;
+	}
+
+	set timeout(val)
+	{
+		this.duration = val;
 	}
 
 }
