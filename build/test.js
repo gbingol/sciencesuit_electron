504b7445f8b6918fe67737a09006b70ed8d5a462
Gökhan Bingöl <gbingol@hotmail.com>
2023-11-25 12:51:35 +0100

    moved util and dom to renderer folder

--- build/test.js
+++ build/test.js
@@ -1,5 +1,5 @@
 const core = require("./sci_dist.js");
 
-const {util} = require("../app/js/util.js")
+const {util} = require("../renderer/util.js")
 
 console.log(util.math.round(core.dnorm(1, 0, 1), 2));
