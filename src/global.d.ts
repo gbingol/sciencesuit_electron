9424d65f91f1ed639ba0d52a566d8769d5fd8de4
Gökhan Bingöl <gbingol@hotmail.com>
2023-12-22 20:46:02 +0100

    types added to global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,12 @@
-import { API } from './preload'
+export const API:
+{
+	dirname: () => string;
+    projdir: () => string;
+    runcmd: (callback: any, cmd: any) => Promise<any>;
+    runpython: (file: any, options: any, isstr?: boolean) => Promise<any>;
+    psychrometry: (k: string[], v: number[]) => Psychrometry;
+    trapz: (x: any, y: any, isCumulative?: boolean) => any;
+}
 
 declare global
 {
