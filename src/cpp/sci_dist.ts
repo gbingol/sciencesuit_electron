9d29534ee46695cbb20eb563303c9335292ecabb
Gökhan Bingöl <gbingol@hotmail.com>
2023-12-29 15:35:31 +0100

    added dist_pf

--- src/cpp/sci_dist.ts
+++ src/cpp/sci_dist.ts
@@ -1,9 +1,20 @@
 var addon = require("./nodebind.node");
 
 
+function dist_pf(
+	x: Array<number> | number,
+	df1: number,
+	df2: number): Array<number> | number
+{
+	
+	if (df1 <=0 || df2<=0)
+		throw "df1>0 and df2>0 expected";
+
+	return addon.dist_pf(x, df1, df2);
+}
 
 
-function dnorm(
+function dist_dnorm(
 	x: Array<number> | number,
 	mean: number = 0,
 	sd: number = 1): Array<number> | number
@@ -17,9 +28,12 @@ function dnorm(
 	if (typeof sd!=="number" || sd <= 0)
 		throw "sd >0 expected";
 
-	return addon.dnorm(x, mean, sd);
+	return addon.dist_dnorm(x, mean, sd);
 }
 
 
 
-module.exports = { dnorm };
+module.exports = {
+	dist_pf,
+	dist_dnorm,
+};
