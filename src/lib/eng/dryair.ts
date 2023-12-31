e5d892fa6c82e193c3ba17cab55d2d24f07903ed
Gökhan Bingöl <gbingol@hotmail.com>
2023-12-31 16:54:23 +0100

    added dryair class

--- src/lib/eng/dryair.ts
+++ src/lib/eng/dryair.ts
@@ -33,52 +33,59 @@ export class Air
 		let T = this.m_T
 		return (1030.5-0.19975*T + 0.00039734*T**2)/1000
 	}
-}
 
-/*
-class Air:
-		
-	def k(self):
-		"""
+	k =()=>
+	{
+		/*
 		returns W/mK, does not take P and Z into account
 		
 		## Reference:
 		Poling et al (2001). Properties of gases and liquids, 5th ed, McGraw-Hill
-		"""
-		T=self._T
-		return (0.002334*T**1.5)/(164.54+T)
-	
-	def conductivity(self):
-		"""Alias for member func k()"""
-		return self.k()
-	
+		*/
+		let T = this.m_T;
+		return (0.002334*T**1.5)/(164.54 + T)
+	}
 
-	def mu(self):
-		"""
+	conductivity = ()=>
+	{
+		//Alias for member func k()
+		return this.k();
+	}
+
+	mu = ()=>
+	{
+		/*
 		returns Pa s, does not take P and Z into account
 		
 		## Reference:
 		Sutherland equation
-		"""
-		T = self._T
+		*/
+		let T = this.m_T;
 		return (1.4592*T**1.5)/(109.1+T)*0.000001
-	
-	def viscosity(self):
-		"""Alias for mu()"""
-		return self.mu()
+	}
 
+	viscosity = () =>
+	{
+		return this.mu();
+	}
 
-	def Pr(self):
-		"""Prandtl number"""
-		return self.cp()*self.mu()/self.k()*1000
-	
+	Pr = ()=>
+	{
+		//Prandtl number
+		return this.cp()*this.mu()/this.k()*1000
+	}
 
-	def rho(self):
-		"""returns kg/m3, uses ideal gas equation with Z"""
-		R = 287.0500676 #J/ (kg K)
-		return self._P/(R*self._T*self._Z)
-	
-	def density(self):
-		"""Alias for rho()"""
-		return self.rho()
-*/
+	rho = ()=>
+	{
+		//returns kg/m3, uses ideal gas equation with Z
+
+		let R = 287.0500676 //J/ (kg K)
+		return this.m_P/(R*this.m_T*this.m_Z)
+	}
+
+	density = ()=>
+	{
+		//Alias for rho()
+		return this.rho()
+	}
+}
