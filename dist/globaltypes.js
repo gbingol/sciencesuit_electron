3f9663f10b83a0a84caa3cb8d65044eea57da800
Gökhan Bingöl <gbingol@hotmail.com>
2024-01-01 11:16:06 +0100

    unstaging *.js

--- dist/globaltypes.js
+++
@@ -1,9 +0,0 @@
-export var stat;
-(function (stat) {
-    let Alternative;
-    (function (Alternative) {
-        Alternative["TWOSIDED"] = "two.sided";
-        Alternative["GREATER"] = "greater";
-        Alternative["LESS"] = "less";
-    })(Alternative = stat.Alternative || (stat.Alternative = {}));
-})(stat || (stat = {}));
